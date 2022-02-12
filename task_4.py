9f22c189c52e16e0caf8dd0842c9a57f4a5aed0e
MGonin <goninmihon@yandex.ru>
2022-02-12 13:40:30 +0300

    Task4 added solve

--- task_4.py
+++ task_4.py
@@ -3,3 +3,13 @@
 # Для решения используйте цикл while и арифметические операции.
 
 number = int(input('Введите число:'))
+
+max_number = 0
+
+while number > 0:
+    current_number = number % 10
+    if current_number > max_number:
+        max_number = current_number
+    number //= 10
+
+print(f'Самая большая цифра = {max_number}')
